206dfef0f53e1dfba8a5767c3661de977b93a23c
Abdelghafour2001 <abdelghafourlahrache@gmail.com>
2023-04-28 19:49:18 +0200

    update files

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Ingredient} from "../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit{
+  ingredients: Ingredient[]=[
+    new Ingredient('Apples',4),
+    new Ingredient('Tomatoes',6)
+  ];
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
 
 }
