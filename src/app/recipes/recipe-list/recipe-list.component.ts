3155591e9da26f848e9a708a208a1fd5aa6541ef
Abdelghafour2001 <abdelghafourlahrache@gmail.com>
2023-05-01 13:46:36 +0200

    update files (services v2)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,13 +9,10 @@ import {RecipeService} from "../../services/recipe.service";
 })
 export class RecipeListComponent implements OnInit{
 recipes!:Recipe[];
-  @Output() recipeWasSelected =  new EventEmitter<Recipe>();
   ngOnInit(){
     this.recipes=this.recipeService.getRecipes();
   }
 constructor(private  recipeService:RecipeService) {
 }
-onRecipeSelected(recipe:Recipe){
-    this.recipeWasSelected.emit(recipe);
-}
+
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,11 +1,12 @@
 import {Recipe} from "../recipes/recipe-list/recipe.model";
+import {EventEmitter} from "@angular/core";
 
 export class RecipeService{
-  recipes: Recipe[] = [
+  private recipes: Recipe[] = [
     new Recipe('A test recipe','idk what to type here ',"https://www.cnet.com/a/img/resize/69256d2623afcbaa911f08edc45fb2d3f6a8e172/hub/2023/02/03/afedd3ee-671d-4189-bf39-4f312248fb27/gettyimages-1042132904.jpg?auto=webp&fit=crop&height=675&width=1200"),
-    new Recipe('A test recipe 2','idk what to type here 2 ',"https://www.cnet.com/a/img/resize/69256d2623afcbaa911f08edc45fb2d3f6a8e172/hub/2023/02/03/afedd3ee-671d-4189-bf39-4f312248fb27/gettyimages-1042132904.jpg?auto=webp&fit=crop&height=675&width=1200")
-
+    new Recipe('A test recipe 2','idk what to type here 2 ',"https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80")
   ];
+  recipeSelected =  new EventEmitter<Recipe>();
   getRecipes(){
     return this.recipes.slice();
   }
